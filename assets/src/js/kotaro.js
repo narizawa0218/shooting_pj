da621debe342795d7d9473097ddeaa4cd5cf42b2
narizawa0218 <narizawa1120006@gmail.com>
2016-03-14 20:46:38 +0900

    不要ファイルを削除

--- assets/src/js/kotaro.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var i, j, str;
-
-  for (i = j = 1; j <= 100; i = ++j) {
-    str = i % 3 === 0 && i % 5 === 0 ? "fizzbuzz" : i % 3 === 0 ? "fizz" : i % 5 === 0 ? "buzz" : i;
-    console.log(str);
-  }
-
-}).call(this);
